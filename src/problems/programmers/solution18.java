9b34780446de68795d4cc5c18869f5a015e454df
KYJ <nananan1213@gmail.com>
2026-02-20 09:57:55 +0900

    feat: 문제 풀이 추가

---
+++ src/problems/programmers/solution18.java
@@ -0,0 +1,15 @@
+package problems.programmers;
+
+public class solution18 {
+    public int solution(String s) {
+        int answer = 0;
+
+        answer = Integer.parseInt(s);
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new solution18().solution("-1234"));
+    }
+}
